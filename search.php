37f640bad5c02c5151890ee44baa329aa2872c4c
Allen Cheung <me@allenc.com>
2011-11-30 22:48:23 -0800

    Pagination controls and styles.

--- search.php
+++ search.php
@@ -29,6 +29,39 @@ global $wp_query;
     </div>
   </article>
   <?php endwhile; ?>
+
+  <?php
+    // $posts_per_page = get_query_var('posts_per_page');
+    $paged = get_query_var('paged');
+    $max_pages = $wp_query->max_num_pages;
+    $curr_link = get_pagenum_link($paged);
+    $next_link = get_next_posts_page_link();
+    $prev_link = get_previous_posts_page_link();
+  ?>
+
+  <?php if ($max_pages > 1): ?>
+    <div class="pagination-buttons clearfix">
+    <?php
+    function get_button($tag, $direction, $disabled, $href, $text) {
+      return "<$tag class=\"$direction pagination-button $disabled\" $href>$text</$tag>";
+    }
+
+    $buttons = '';
+    if ($paged < $max_pages) {
+      $buttons .= get_button('a', 'prev', '', "href=\"$next_link\"", 'Older');
+    } else {
+      $buttons .= get_button('span', 'prev', 'disabled', '', 'Older');
+    }
+    if ($paged > 1) {
+      $buttons .= get_button('a', 'next', '', "href=\"$prev_link\"", 'Newer');
+    } else {
+      $buttons .= get_button('span', 'next', 'disabled', '', 'Newer');
+    }
+    echo $buttons;
+    ?>
+   </div>
+  <?php endif; ?>
+
 <?php else: ?>
   <article class="post" id="no-search-results">
     <div class="post-body">
