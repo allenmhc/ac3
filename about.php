45a0e9abfef1a94c8974f87c3b0ba867749c8029
Allen Cheung <me@allenc.com>
2012-01-04 00:58:48 -0800

    About me banner.

--- about.php
+++ about.php
@@ -10,6 +10,16 @@ define('WP_USE_THEMES', false);
 ?>
 
 <div id="alpha">
+  <section id="about-banner">
+    <img id="about-banner-image" src="<?php ac3_images_dir(); ?>/me_banner.jpg" alt="Looking lost" />
+    <div id="about-text">
+      <h1>ac.about</h1>
+      <p>
+        I'm a front-end web software engineer who cares about a clean design, both
+        in the user interface and the code its built upon.
+      </p>
+    </div>
+  </section>
 </div>
 
 <div id="beta">
